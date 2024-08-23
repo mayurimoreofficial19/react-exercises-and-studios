f4264db3b0b5cce43a60bb4e78136b677ec6da37
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-08-22 21:22:11 -0500

    studio react-part-one is completed

--- studio/src/App.jsx
+++ studio/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <div>
+        <div className="recipePhotoBlock">
           <RecipePhoto />
           <div>
             <RecipeDescription />
--- studio/src/components/Photos.jsx
+++ studio/src/components/Photos.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./description.module.css";
+import styles from "./Description.module.css";
 
 let RecipePhoto = () => {
   return (
