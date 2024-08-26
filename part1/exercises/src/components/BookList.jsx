780ab91447950e9d6f30bf828e66bcf476a531a3
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-08-25 21:40:34 -0500

    REact exercise part 1

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,18 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+  let pageTitle = "Must-Read Books for 2024";
+  let book1 =
+    "https://m.media-amazon.com/images/I/41lJNXRfFdL._SY445_SX342_.jpg";
+  let book2 =
+    "https://m.media-amazon.com/images/I/41qqSRuzrBL._SY445_SX342_.jpg";
+  let book3 =
+    "https://m.media-amazon.com/images/I/41GNuyCLkpL._SY445_SX342_.jpg";
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
+  return (
+    <div>
+      <h3>Must-Read Books for 2024</h3>
+      <img src={book1} alt="you become what you think" />
+      <img src={book2} alt="The mind of the leader" />
+      <img src={book3} alt="The universe always has a plan" />
+    </div>
+  );
 }
